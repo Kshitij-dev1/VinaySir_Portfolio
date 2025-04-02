04d46a185997bd3d72126475c5468750c4de17b6
Kshitij-dev1 <Kshitij95official@gmail.com>
2025-04-02 18:11:22 +0530

    1st push

---
+++ vpJS.js
@@ -0,0 +1,32 @@
+//   Testimonials==============
+var swiper = new Swiper(".mySwiperTest", {
+    slidesPerView: 1,
+    spaceBetween: 10,
+    loop: true,
+    autoplay: {
+      delay: 3000, // Auto-slide every 3 seconds
+      disableOnInteraction: false, // Keeps autoplay active after user interaction
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+      //   dynamicBullets: true,
+    },
+    centeredSlides: true, // Center the active slide
+
+    slidesPerGroup: 1, // Moves slides one by one
+    speed: 1000, // Smooth transition duration (in ms)
+    effect: "slide",
+    breakpoints: {
+      640: {
+        slidesPerView: 2,
+        spaceBetween: 20,
+      },
+
+      1024: {
+        slidesPerView: 3 /*Product per slide*/,
+        spaceBetween: 20 /*Gap*/,
+      },
+    },
+  });
+
